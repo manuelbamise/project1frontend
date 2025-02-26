0417168a322c31ca2aa427ce5278fad826a53b07
manuelbamise <etobi1930@gmail.com>
2025-02-26 15:51:28 +0100

    process: making plans on the project... first going to wrok on the backend..finding the web scraper i'll be using, the db to store the data and how i'll be piecing together all the parts of the application

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 const text = 'hello world'
 const identy = 'dao12'
-
+const color = '#44'
 </script>
 
 <template>
+  <div :title="text">hi i'm a message here</div>
   <div :id="identy">this is the div for a column..</div>
-  <div>this page is just to tell you <span v-html="text"></span></div>
+  <div :style="{ color }">this page is just to tell you <span v-html="text"></span></div>
 </template>
 
 <style scoped>
