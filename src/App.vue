376ba04c8696e4a0a68d29588c28ac4ded167826
manuelbamise <etobi1930@gmail.com>
2025-02-26 15:06:48 +0100

    process: initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const text = 'hello world'
+const identy = 'dao12'
+
+</script>
+
+<template>
+  <div :id="identy">this is the div for a column..</div>
+  <div>this page is just to tell you <span v-html="text"></span></div>
+</template>
+
+<style scoped>
+#dao12 {
+  color: aqua;
+}
+</style>
