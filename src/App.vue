b670e650e8a755db0c8e0c864ba181560d859ed0
manuelbamise <etobi1930@gmail.com>
2025-03-21 20:36:23 +0100

    process: this is another test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <header>
+        <img
+            alt="Vue logo"
+            class="logo"
+            src="./assets/logo.svg"
+            width="125"
+            height="125"
+        />
 
-    <div class="wrapper">
-      <div class=" text-blue-300">hello world from manuelbamise</div>
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        <div class="wrapper">
+            <div class="text-blue-300">hello world from manuelbamise</div>
+            <HelloWorld msg="You did it!" />
+        </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <TheWelcome />
+        my name is emmanuel &COPY;
+    </main>
 </template>
